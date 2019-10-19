dcc4071e63b980a273be6b55b43ae5e6a3eb3399
rsreeram17 <41049719+rsreeram17@users.noreply.github.com>
2019-10-19 17:45:54 +0530

    Update Data batching.py
    Final edits on dataloader wrapping

--- Data batching.py
+++ Data batching.py
@@ -3,6 +3,8 @@ import torch.utils.data as data
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence as PACK
+from torch.nn.utils.rnn import pad_packed_sequence
 
 class Datasetclass (data.Dataset): 
     def __init__(self,data_list,word_to_ix,caselookup):
@@ -53,5 +55,30 @@ def collate_fn (data):
 
 train_data_loader = torch.utils.data.DataLoader(dataset = dataset,batch_size=10,collate_fn=collate_fn)
 
+class LSTMTagger(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim,batch_first = True)
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        
+    def forward(self,sentences,lengths):
+        
+        x_packed = PACK(sentences,lengths, batch_first=True)
+        output_packed, _ = self.lstm(x_packed)
+        output_padded, output_lengths = pad_packed_sequence(output_packed, batch_first=True)
+        tag_space = self.hidden2tag(output_padded)
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
+
+model = LSTMTagger(105,512,23623,10)
+sentences,lengths,labels = next(iter(train_data_loader))
+op = model(sentences,lengths)
+
+batch_loss = 0
+for i in range (op.size(0)):
+    nllloss = F.nll_loss(op[i],labels[i],ignore_index=9)
+    batch_loss += nllloss
+        
 
    
