640ffbbf9d5bca2d87061b4218c5c66bfb5ac16e
rsreeram17 <41049719+rsreeram17@users.noreply.github.com>
2019-10-20 15:49:47 +0530

    Script to run the modelling

---
+++ run_script.py
@@ -0,0 +1,44 @@
+from data_import import *
+from data_batching import *
+from Model import *
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+caselookup = {'numeric':0,'alllower':1,'allupper':2,'initupper':3,'others':4}
+labels_lookup = {'B-ORG':0,'O':1,'B-MISC':2,'B-PER':3,'I-PER':4,'B-LOC':5,'I-ORG':6,'I-MISC':7,'I-LOC':8}
+
+##Importing the necessary data
+sentences_labels,sentences_casing = get_data("train.txt",dir_path,caselookup,labels_lookup)
+sentences_labels_val,sentence_casing_val = get_data("valid.txt",dir_path,caselookup,labels_lookup)
+sentences_labels_test,sentence_casing_test = get_data("test.txt",dir_path,caselookup,labels_lookup)
+word_to_ix = get_word_to_ix(sentences_labels,sentences_labels_val,sentences_labels_test)
+training_data = get_training_data(sentences_labels,sentences_casing,word_to_ix)      
+validation_data = get_training_data(sentences_labels_val,sentence_casing_val,word_to_ix)
+testing_data = get_training_data(sentences_labels_test,sentence_casing_test,word_to_ix)
+
+##Dataset preparation
+dataset_training = Datasetclass(training_data,word_to_ix,caselookup)
+dataset_val = Datasetclass(validation_data,word_to_ix,caselookup)
+train_data_loader = torch.utils.data.DataLoader(dataset = dataset_training,batch_size=256,collate_fn=collate_fn)
+val_data_loader = torch.utils.data.DataLoader(dataset = dataset_val,batch_size=256,collate_fn=collate_fn)
+data_loaders = {}
+data_loaders['train'] = train_data_loader
+data_loaders['val'] = val_data_loader
+data_lengths = {}
+data_lengths['train'] = len(dataset_training)
+data_lengths['val'] = len(dataset_val)
+
+##Model building
+model = LSTMTagger(105,128,len(word_to_ix),10)
+optimizer_ft = torch.optim.Adam(model.parameters(), lr=0.001,  betas=(0.9, 0.99), weight_decay=0.0002)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+num_epochs = 10
+model = train_model(model,optimizer_ft,exp_lr_scheduler,num_epochs,dir_path,data_loaders)
+
+
+
+
+
+
+
