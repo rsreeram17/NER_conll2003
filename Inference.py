87a9cf15988000235ac15730469d580f27384b7b
rsreeram17 <41049719+rsreeram17@users.noreply.github.com>
2019-10-23 23:03:17 +0530

    Updated fscore calc

--- Inference.py
+++ Inference.py
@@ -10,5 +10,11 @@ path = dir_path+"\\"+model_name
 checkpoint = torch.load(path)
 model.load_state_dict(checkpoint['model_state_dict'])
 
-average_precision,average_recall,fscore,precision_list,recall_list = testing(testing_data,model,word_to_ix,caselookup)
+average_precision,average_recall,fscore,precision_list,recall_list,labels_combined,pred_combined = testing (testing_data,model,word_to_ix,caselookup)
 
+
+f_score_list = []
+for i in range(5):
+    f_score_item = (2*precision_list[i]*recall_list[i])/(precision_list[i] + recall_list[i])
+    f_score_list.append(f_score_item)
+ 
